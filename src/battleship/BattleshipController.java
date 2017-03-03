25f05970a37f658fcdae38e33e730d57a2840977
Arturo Cordero <vakmancordero@gmail.com>
2017-03-02 18:43:39 -0600

    Inicializando proyecto

---
+++ src/battleship/BattleshipController.java
@@ -0,0 +1,18 @@
+package battleship;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author VakSF
+ */
+public class BattleshipController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+}
